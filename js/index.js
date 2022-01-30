089a1730a139dc0121383e8bc3041ea7a383adb7
priscahlimo <priscahlimoh@gmail.com>
2022-01-30 19:07:37 +0300

    add conversions

--- js/index.js
+++ js/index.js
@@ -32,14 +32,16 @@ function getTime(timestamp){
 // Getting weather updates through search city
 
 function handleTemp (response){
-   console.log(response.data)
+   
+    currentTemperature=response.data.main.temp
 
-
-    document.querySelector(".temp").innerHTML = Math.round(response.data.main.temp) + "°C"
+    document.querySelector(".temp").innerHTML = Math.round(currentTemperature) 
     document.querySelector(".city-name").innerHTML = response.data.name
     document.querySelector("#humid").innerHTML=response.data.main.humidity+"%"
     document.querySelector("#windy").innerHTML=response.data.wind.speed +"km/h"
     document.querySelector("#prep").innerHTML=response.data.weather[0].description
+
+
     let dateElement=document.querySelector("#date")
     dateElement.innerHTML= getTime(response.data.dt*1000)
     let icons=document.querySelector("#icon");
@@ -55,55 +57,39 @@ search(cityElement)
 
 function search(city) {
     let key = "d987c3e70953ea07826bfce27d61b157";
-    // let city=document.querySelector("#search-input").value
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
     axios.get(url).then(handleTemp);
 }
  
-search("Kenya")
+function convertToFaren(event){
+    event.preventDefault();
+    convert.classList.add("active")
+    converted.classList.remove("active")
+    let nowDeg =document.querySelector(".temp")
+    let convertLink = Math.round( currentTemperature* 9/5) + 32
+    nowDeg.innerHTML= convertLink
+  
+}
+
+function convertToDeg(event){
+    event.preventDefault();
+    converted.classList.add("active")
+    convert.classList.remove("active")
+    let nowFarhent=document.querySelector(".temp")
+    nowFarhent.innerHTML=Math.round(currentTemperature)  
+}
+
+let currentTemperature= null;
 
 let form=document.querySelector("#submission")
 form.addEventListener("submit", getTemperature)
 
 
+let convert=document.querySelector("#farhent-link");
+convert.addEventListener("click" , convertToFaren)
 
+let converted=document.querySelector("#celcius-link");
+converted.addEventListener("click" , convertToDeg) 
 
-    
-
-
-
-    // getTemperature("kenya");
-
-    // getting response through coodinates
-
-    // function getByCoords(position){
-    //     let key = "d987c3e70953ea07826bfce27d61b157"
-    //     let latitudes=position.coords.latitude
-    //     let longitudes=position.coords.longitude
-    //     let endpoint = "http://api.openweathermap.org/data/2.5/weather?"
-    //     let lurl = `${endpoint}lat=${latitudes}&lon=${longitudes}&appid=${key}&units=metric`
-       
-       
-    // }
-
-    // navigator.geolocation.getCurrentPosition(getByCoords)
-
-    // // let searchCoords = document.querySelector("#search-current")
-    // // searchCoords.addEventListener("click", getByCoords)
-
-    // function search(city) {
-    //     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    //     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    //     axios.get(apiUrl).then(displayTemperature);
-    //   }
-      
-    //   function handleSubmit(event) {
-    //     event.preventDefault();
-    //     let cityInputElement = document.querySelector("#city-input");
-    //     search(cityInputElement.value);
-    //   }
-      
-   
-      
-    
+search("Kenya")
